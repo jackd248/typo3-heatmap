baa78f0eab3c6bc4ebcd9f7645f5c973ac420b14
Konrad Michalik <konrad.michalik@gmx.de>
2025-08-19 16:24:03 +0200

    fix: update extension name references

--- Classes/Widgets/Provider/ContentChangesHistoryDataProvider.php
+++ Classes/Widgets/Provider/ContentChangesHistoryDataProvider.php
@@ -3,7 +3,7 @@
 declare(strict_types=1);
 
 /*
- * This file is part of the TYPO3 CMS extension "typo3_heatmap_widget".
+ * This file is part of the TYPO3 CMS extension "typo3_heatmap".
  *
  * Copyright (C) 2025 Konrad Michalik <hej@konradmichalik.dev>
  *
@@ -21,7 +21,7 @@ declare(strict_types=1);
  * along with this program. If not, see <https://www.gnu.org/licenses/>.
  */
 
-namespace KonradMichalik\Typo3HeatmapWidget\Widgets\Provider;
+namespace KonradMichalik\Typo3Heatmap\Widgets\Provider;
 
 use Doctrine\DBAL\ParameterType;
 use TYPO3\CMS\Backend\Routing\Exception\RouteNotFoundException;
